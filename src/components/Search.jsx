42ee91021aeaa7c75479e7197efaaa6b9b231bad
Pandhu Arya <pandhu.munjalindra@gmail.com>
2024-03-05 08:53:50 +0700

    feat: added component reuseable search input

---
+++ src/components/Search.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const SearchInput = ({ search, setSearch }) => {
+    return (
+      <input
+        type="search"
+        name="Search"
+        placeholder="Search..."
+        className="w-56 py-2 pl-4 text-sm outline-none bg-white text-gray-700 rounded-lg shadow border border-gray-300 focus:border-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+    );
+  };
+  
+
+export default SearchInput;
