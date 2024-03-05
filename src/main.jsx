38a94bf78a2c63e8f22d6fb3ce7449c684812c77
Pandhu Arya <pandhu.munjalindra@gmail.com>
2024-03-06 05:48:28 +0700

    feat: added css to react toast

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import "react-toastify/dist/ReactToastify.css";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import React from "react";
 import App from "./App";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
