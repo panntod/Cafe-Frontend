89b033bdd038f22a16cf91ee2de8f7a3ea97a236
Pandhu Arya <pandhu.munjalindra@gmail.com>
2024-03-05 08:44:27 +0700

    feat: added component reuseable alert

---
+++ src/components/Alert.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import { Alert } from "@material-tailwind/react";
+
+function CustomAlert({ message }) {
+  const [open, setOpen] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setOpen(false);
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <Alert
+      open={open}
+      color="red"
+      animate={{
+        mount: { y: 0 },
+        unmount: { y: 100 },
+      }}
+    >
+      {message}
+    </Alert>
+  );
+}
+
+export default CustomAlert
