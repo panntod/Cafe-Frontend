e8da57e767016beeec093da52a33f1854d17f61e
Pandhu Arya <pandhu.munjalindra@gmail.com>
2024-03-05 09:07:10 +0700

    feat: added not found page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { CustomButton } from "../components";
+
+const NotFoundPage = () => {
+  const handleBack = () => {
+    window.history.go(-1);
+  };
+
+  const handleBackHome = () => {
+    const role = JSON.parse(localStorage.getItem("role"));
+    role ? window.location.href = `/${role}` : window.location.href = "/"
+  };
+  return (
+    <section className="bg-white dark:bg-gray-900 ">
+      <div className="container flex items-center min-h-screen px-6 py-12 mx-auto">
+        <div className="flex flex-col items-center max-w-sm mx-auto text-center">
+          <p className="p-3 text-sm font-medium text-blue-500 rounded-full bg-blue-50 dark:bg-gray-800">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
+              />
+            </svg>
+          </p>
+          <h1 className="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">
+            Page not found
+          </h1>
+          <p className="mt-4 text-gray-500 dark:text-gray-400">
+            Page yang anda cari seperti nya tidak ada. Cek kembali penulisan
+            link anda
+          </p>
+
+          <div className="flex items-center w-full mt-6 gap-x-3 shrink-0 sm:w-auto">
+            <CustomButton
+              className="flex items-center justify-center text-gray-700 bg-white border gap-x-2 dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700"
+              onClick={handleBack}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-5 h-5 rtl:rotate-180"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
+                />
+              </svg>
+              Go Back
+            </CustomButton>
+
+            <CustomButton color={"blue"} onClick={handleBackHome}>
+              Take me home
+            </CustomButton>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default NotFoundPage;
